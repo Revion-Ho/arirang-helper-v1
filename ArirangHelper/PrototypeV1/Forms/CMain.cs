cdb8e09c1f6c41c29f3b2ae570935e0ed8d20f90
Revion <revion.ho@gmail.com>
2024-12-30 17:32:33 +0900

    기본 프로젝트 업로드

---
+++ ArirangHelper/PrototypeV1/Forms/CMain.cs
@@ -0,0 +1,11 @@
+﻿namespace PrototypeV1.Forms
+{
+    public partial class CMain : Form
+    {
+        public CMain()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
